36a3af3787d01fe3854b6fc2160a85f4f5d25035
sanket9921 <sanketlike63@gmail.com>
2025-03-28 14:30:22 +0530

    chage ip

--- mocktest-frontend/src/api/axios.js
+++ mocktest-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  // baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "http://164.92.115.205:5000/api",
   withCredentials: true,
 });
 
