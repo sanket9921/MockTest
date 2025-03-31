77d350075820e11ff021763313998b0e1c135c90
sanket9921 <sanketlike63@gmail.com>
2025-03-31 08:43:57 +0530

    modifed axios now it use .env file

--- mocktest-frontend/src/api/axios.js
+++ mocktest-frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://127.0.0.1:5000/api",
-  baseURL: "https://practicetest.bytrait.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
